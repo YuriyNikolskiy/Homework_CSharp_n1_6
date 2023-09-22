79e87b7eaac457e4f0ac5e9bba7116e86ab024bc
Yuriy Nikolskiy <bankirerbe@gmail.com>
2023-09-23 01:29:07 +0300

    first commit

---
+++ Program_Task6.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a number: ");
+string number = Console.ReadLine();
+int number1 = Int32.Parse(number);
+
+if(number1 % 2 == 0)
+{
+    Console.WriteLine("The number is even");    
+}
+else
+{
+    Console.WriteLine("The number is odd");    
+}
